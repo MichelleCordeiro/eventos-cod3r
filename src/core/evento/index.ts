7a310a6f777d3a64922b3596e3937642dfc645ab
Michelle Cordeiro <mic321@gmail.com>
2024-12-04 00:57:55 -0300

    chore: import functions in index core

--- src/core/evento/index.ts
+++ src/core/evento/index.ts
@@ -1,5 +1,18 @@
 import Evento from "./model/Evento"
 import Convidado from "./model/Convidado"
 
+import criarEventoVazio from "./functions/criarEventoVazio"
+import complementarEvento from './functions/complementarEvento'
+
+import criarConvidadoVazio from './functions/criarConvidadoVazio'
+import complementarConvidado from './functions/complementarConvidado'
+
 export type { Evento, Convidado }
 // precisa do export TYPE pq ta exportando 2 interfaces
+
+export {
+  criarEventoVazio,
+  complementarEvento,
+  criarConvidadoVazio,
+  complementarConvidado,
+}
