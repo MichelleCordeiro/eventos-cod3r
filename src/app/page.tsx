8a79ad0d34fae7e13f65567ab88ca2b0429c2456
Michelle Cordeiro <mic321@gmail.com>
2024-12-04 04:34:34 -0300

    feat: add initial routes evento and convite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import Pagina from '@/components/template/Pagina'
-import { Evento } from '@/core'
+import Logo from '@/components/template/Logo'
+import Link from 'next/link'
 
 export default function Home() {
-  const e: Partial<Evento> = {}
   return (
-    <Pagina className='w-full'>
-      <div>Inicio</div>
-    </Pagina>
-  )
+    <div className="h-screen flex flex-col items-center justify-center gap-10 bg-[url('/background-elementos.svg')] bg-cover bg-no-repeat bg-center">
+      <div className='flex flex-col items-center gap-4'>
+        <Logo classes='logo-large' />
+        <p className='w-96 text-zinc-500 font-light text-center leading-6 select-none'>
+          Crie e gerencie o convite do seu evento de forma rápida e fácil, sem complicações!
+        </p>
+      </div>
+      <Link href='/evento' className='button blue text-lg uppercase'>
+        Crie o seu Evento
+      </Link>
+    </div>
+  );
 }
--- src/components/template/Logo.tsx
+++ src/components/template/Logo.tsx
@@ -2,16 +2,29 @@ import { Righteous } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const fonte = Righteous({
+const font = Righteous({
   subsets: ["latin"],
   weight: "400"
 })
 
-export default function Logo() {
+interface LogoProps {
+  classes?: string;
+  adjustedImgSize?: number;
+}
+
+export default function Logo({
+  classes = 'logo-small',
+  adjustedImgSize = 50
+}: LogoProps) {
+  const ImgSize = classes === 'logo-large' ? 100 : adjustedImgSize;
+
   return (
-    <Link href='/' className={`${fonte.className} flex items-center gap-2`}>
-      <Image src='/logo.svg' width={50} height={50} alt='Logo' />
-      <h1 className='leading-5 flex-col items-center text-lg'>
+    <Link
+      href='/'
+      className={`${font.className} ${classes}`}
+    >
+      <Image src='/logo.svg' width={ImgSize} height={ImgSize} alt='Logo' />
+      <h1 className={`flex ${classes === 'logo-small' ? 'flex-col' : 'flex-row'}`}>
         <div>
           CONVIT
           <span className='text-blue-500'>3</span>
@@ -19,5 +32,5 @@ export default function Logo() {
         <div>DIGITAL</div>
       </h1>
     </Link>
-  )
+  );
 }
