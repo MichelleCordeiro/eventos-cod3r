9f798a73ed695bd78bcb1de430ffdd7c375cd26e
Michelle Cordeiro <mic321@gmail.com>
2024-12-04 21:30:10 -0300

    feat: admin get evento statistics

--- src/app/(paginas)/convite/[alias]/page.tsx
+++ src/app/(paginas)/convite/[alias]/page.tsx
@@ -1,10 +1,9 @@
 export default async function PaginaConvite(props: any) {
   const params = await props.params
-  console.log("PaginaConvite: ", props)
+  // console.log("PaginaConvite: ", props)
 
   return (
     <div>
-      {/* <span>{props.params.alias}</span> */}
       <span>{params.alias}</span>
     </div>
   )
--- src/app/(paginas)/evento/admin/[...todos]/page.tsx
+++ src/app/(paginas)/evento/admin/[...todos]/page.tsx
@@ -1,13 +1,40 @@
-export default async function PaginaAdminEvento(props: any) {
-  const params = await props.params
+'use client'
+
+import { Convidado, Evento, eventos } from '@/core'
+import { use, useEffect, useState } from 'react'
+
+export default function PaginaAdminEvento(props: any) {
+  const params: any = use(props.params)
 
   const id = params.todos[0]
-  const senha = params.todos[1] // a senha será passada na url apenas durante a desenvolvimento. Na aplicação, a senha será solicitada, numa nova tela, na hora de acessar algum evento
+  const [evento, setEvento] = useState<Evento | null>(null)
+  const [senha, setSenha] = useState<String | null>(params.todos[1] ?? null)
+  // a senha será passada na url apenas durante a desenvolvimento. Na aplicação, a senha será solicitada, numa nova tela, na hora de acessar algum evento
 
-  return (
-    <div className='flex flex-col'>
-      <span>Id: {id}</span>
-      <span>Senha: {senha}</span>
-    </div>
+  const presentes = evento?.convidados.filter(c => c.confirmado) ?? []
+  const ausentes = evento?.convidados.filter(c => !c.confirmado) ?? []
+  const totalGeral = evento?.convidados.reduce(
+    (total: number, convidado: Convidado) => {
+      return total + convidado.qtdeAcompanhantes + 1
+      // return total + (convidado.confirmado ? convidado.qtdeAcompanhantes + 1 : 0)
+    }, 0
   )
+
+  function carregarEvento() {
+    const evento = eventos.find((ev) => ev.id === id && ev.senha === senha)
+    setEvento(evento ?? null)
+  }
+
+  // sempre q o id ou a senha mudarem ele carregar novamente um evento
+  useEffect(() => {
+    carregarEvento()
+  }, [id, senha])
+
+  console.log('presentes: ', presentes, 'ausentes: ', ausentes, 'totalGeral: ', totalGeral)
+
+  return evento ? (
+    <div className='flex flex-col'>
+      <span>{evento.nome}</span>
+    </div>
+  ) : null
 }
